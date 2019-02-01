29a1fd46c08ed6ed2ad7f903d30a5f847c61221f
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-01 16:38:35 +0800

    Create start.js

---
+++ start.js
@@ -0,0 +1,14 @@
+{
+  "name": "alexbot",
+  "version": "1.0.0",
+  "main": "start.js",
+  "scripts": {
+    "start": "node start.js"
+  },
+  "author": "Justin",
+  "license": "ISC",
+  "description": "",
+  "dependencies": {
+    "dblapi.js": "^2.3.0"
+  }
+}
